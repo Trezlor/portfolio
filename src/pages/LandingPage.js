3361ec294f54f038d390048cb427f1e381efc3fa
Sondre <sondre-h@hotmail.com>
2022-09-02 13:24:13 +0200

    hoi

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,7 +1,43 @@
+import Sidebar from "./landingPage/Sidebar";
+
 export default function LandingPage() {
 	return (
 		<>
-			<div></div>
+			<Sidebar />
+			<h1>yo</h1>
+			<h1>yo</h1>
+			<h1>yo</h1>
+			<h1>yo</h1>
+			<h1>yo</h1>
+			<h1>yo</h1>
+			<h1>yo</h1>
+			<h1>yo</h1>
+			<h1>yo</h1>
+			<h1>yo</h1>
+			<h1>yo</h1>
+			<h1>yo</h1>
+			<h1>yo</h1>
+			<h1>yo</h1>
+			<h1>yo</h1>
+			<h1>yo</h1>
+			<h1>yo</h1>
+			<h1>yo</h1>
+			<h1>yo</h1>
+			<h1>yo</h1>
+			<h1>yo</h1>
+			<h1>yo</h1>
+			<h1>yo</h1>
+			<h1>yo</h1>
+			<h1>yo</h1>
+			<h1>yo</h1>
+			<h1>yo</h1>
+			<h1>yo</h1>
+			<h1>yo</h1>
+			<h1>yo</h1>
+			<h1>yo</h1>
+			<h1>yo</h1>
+			<h1>yo</h1>
+			<h1>yo</h1>
 		</>
 	);
 }
---
+++ src/pages/landingPage/sidebar/data.js
@@ -0,0 +1,12 @@
+export const SidebarData = [
+	{
+		title: "Home",
+		icon: <icon />,
+		link: "/home",
+	},
+	{
+		title: "hoi",
+		icon: <icon />,
+		link: "/hoi",
+	},
+];
